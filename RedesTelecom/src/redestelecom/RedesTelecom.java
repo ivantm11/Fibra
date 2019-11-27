41a793129a5e0ef3d071b8f5dec0fe099cea4cf8
Ivan Trejo <ivantm11@icloud.com>
2019-11-26 21:13:16 -0600

    NEW: Main menu added

--- RedesTelecom/src/redestelecom/RedesTelecom.java
+++ RedesTelecom/src/redestelecom/RedesTelecom.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package redestelecom;
 
 import java.sql.Connection;
@@ -13,14 +8,12 @@ import java.sql.Connection;
  */
 public class RedesTelecom {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         ConexionSQL BaseSQL = new ConexionSQL();
         Connection BaseDeDatos = BaseSQL.getConexionSQL();
         if (BaseDeDatos != null) {
-            
+            MenuPrincipal menuNuevo = new MenuPrincipal(BaseDeDatos);
+            menuNuevo.setVisible(true);
         }
     }
 }
